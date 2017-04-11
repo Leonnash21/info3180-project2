256b44258bea1384918e25290e886ce01a6e947d
leonnash21 <leonnash2008@gmail.com>
2017-04-11 08:48:14 +0000

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj1:password123@localhost/proj1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj2:password123@localhost/proj2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['DEFAULT_ADMIN'] = 'Admin <admin@example.com>'
 
